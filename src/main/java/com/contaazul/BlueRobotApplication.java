e3cfab4db5033b6723ab0066001c601681b690fd
Breno Marques <breno@brolam.com.br>
2017-06-03 07:41:52 -0300

    Inicio do projeto, closed #1

---
+++ src/main/java/com/contaazul/BlueRobotApplication.java
@@ -0,0 +1,12 @@
+package com.contaazul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueRobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueRobotApplication.class, args);
+	}
+}
